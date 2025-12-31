9b6cd7f6fd4fb24009cfd69bb3e47cf0d9e2711a
chuanhao01 <34702921+chuanhao01@users.noreply.github.com>
2026-01-01 03:08:17 +0800

    [WIP] dark mode works, with WIP form

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,12 +1,30 @@
+import { ThemeProvider } from "@/components/theme-provider";
+import "./globals.css";
+import { ModeToggle } from "@/components/theme-toggle-button";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html>
-      <body>
-        {children}
+    <html suppressHydrationWarning>
+      <body className="flex min-h-screen flex-col">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <div className="z-50 flex h-13 w-full items-center p-4 lg:sticky lg:top-0">
+            <p>r/NUS</p>
+            <div className="ml-auto">
+              <ModeToggle />
+            </div>
+          </div>
+          <div className="hidden h-13 lg:block"></div>
+          <div>{children}</div>
+        </ThemeProvider>
       </body>
     </html>
   );
---
+++ frontend/app/register/page.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import {
+  Field,
+  FieldDescription,
+  FieldError,
+  FieldLabel,
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Controller, useForm } from "react-hook-form";
+import * as z from "zod";
+
+const formSchema = z.object({
+  username: z
+    .string()
+    .min(5, "Bug title must be at least 5 characters.")
+    .max(32, "Bug title must be at most 32 characters."),
+  email: z
+    .string()
+    .min(5, "Description must be at least 20 characters.")
+    .max(100, "Description must be at most 100 characters."),
+  password: z
+    .string()
+    .min(5, "Bug title must be at least 5 characters.")
+    .max(32, "Bug title must be at most 32 characters."),
+});
+
+export default function Register() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(data: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    console.log(data);
+  }
+
+  return (
+    <div className="flex min-h-screen w-full justify-center lg:items-center">
+      <div className="flex items-center justify-center">
+        <Controller
+          name="username"
+          control={form.control}
+          render={({ field, fieldState }) => (
+            <Field data-invalid={fieldState.invalid}>
+              <FieldLabel htmlFor={field.name}>Bug Title</FieldLabel>
+              <Input
+                {...field}
+                id={field.name}
+                aria-invalid={fieldState.invalid}
+                placeholder="Login button not working on mobile"
+                autoComplete="off"
+              />
+              <FieldDescription>
+                Provide a concise title for your bug report.
+              </FieldDescription>
+              {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+            </Field>
+          )}
+        />
+      </div>
+    </div>
+  );
+}
