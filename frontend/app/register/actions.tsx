966d3936f933234cf2f53a945afe54b46764057b
chuanhao01 <34702921+chuanhao01@users.noreply.github.com>
2026-01-06 22:35:41 +0800

    [feat] register page
    Description
    Need to still add redirect to RegisterSuccessful

---
+++ frontend/app/register/actions.tsx
@@ -0,0 +1,71 @@
+"use server";
+
+import * as z from "zod";
+import { formSchema } from "./page";
+
+/**
+ * Calls the BE to register the user
+ * @param data passed from form.handleSubmit()
+ * @returns
+ */
+export async function register(
+  data: z.infer<typeof formSchema>,
+): Promise<boolean> {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/createUserByUsernameAndEmail`,
+      {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: { "Content-Type": "application/json" },
+      },
+    );
+    if (!res.ok) {
+      console.warn(
+        `createUserByUsernameAndEmail failed due to: ${await JSON.stringify(res.json())}`,
+      );
+    }
+    return res.ok;
+  } catch {
+    console.warn("createUserByUsernameAndEmail fetch call failed");
+    return false;
+  }
+}
+
+export async function checkUsername(username: string): Promise<boolean> {
+  try {
+    const res = await fetch(`${process.env.BACKEND_URL}/checkUsername`, {
+      method: "POST",
+      body: JSON.stringify({ username: username }),
+      headers: { "Content-Type": "application/json" },
+    });
+    if (!res.ok) {
+      return false;
+    }
+    const data = (await res.json()) as { isUnique: boolean };
+    return data.isUnique;
+  } catch {
+    // If fetch fails
+    console.warn("checkUsername fetch call failed");
+    return false;
+  }
+}
+
+export async function checkEmail(email: string): Promise<boolean> {
+  try {
+    const res = await fetch(`${process.env.BACKEND_URL}/checkEmail`, {
+      method: "POST",
+      body: JSON.stringify({ email: email }),
+      headers: { "Content-Type": "application/json" },
+    });
+    if (!res.ok) {
+      return false;
+    }
+    const data = (await res.json()) as { isUnique: boolean };
+    return data.isUnique;
+  } catch {
+    // If fetch fails
+    console.warn("checkEmail fetch call failed");
+    return false;
+  }
+}
--- frontend/app/register/page.tsx
+++ frontend/app/register/page.tsx
@@ -18,128 +18,163 @@ import * as z from "zod";
 import { useState } from "react";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Label } from "@/components/ui/label";
+import { checkUsername, checkEmail, register } from "./actions";
+import { Dialog, DialogContent, DialogHeader } from "@/components/ui/dialog";
+import { DialogDescription, DialogTitle } from "@radix-ui/react-dialog";
+import { Spinner } from "@/components/ui/spinner";
 
-const formSchema = z.object({
+export const formSchema = z.object({
   username: z
     .string()
-    .min(5, "Bug title must be at least 5 characters.")
-    .max(32, "Bug title must be at most 32 characters."),
+    .min(3, "Username must be at least 3 characters.")
+    .max(50, "Username is too long")
+    .refine(checkUsername, { error: "Username already taken" }),
   email: z
-    .string()
-    .min(5, "Description must be at least 20 characters.")
-    .max(100, "Description must be at most 100 characters."),
+    .email()
+    .min(10, "Email must be at least 10 characters.")
+    .max(200, "Email is too long")
+    .refine(checkEmail, { error: "Email already used" }),
   password: z
     .string()
-    .min(5, "Bug title must be at least 5 characters.")
-    .max(32, "Bug title must be at most 32 characters."),
+    .min(5, "Password must be atleast 5 characters")
+    .max(50, "Password is too long"),
 });
 
 export default function Register() {
+  const formId = "register-form";
+  const [disabled, setDisabled] = useState(false);
   const form = useForm<z.infer<typeof formSchema>>({
+    mode: "onBlur",
     resolver: zodResolver(formSchema),
     defaultValues: {
       username: "",
       email: "",
       password: "",
     },
+    disabled,
+  });
+  const formSubmitHandler = form.handleSubmit(async (data) => {
+    setDisabled(true);
+    // if register was unsuccessful
+    if (!(await register(data))) {
+      setIsRegisterErrorDialogOpen(true);
+    }
+    setDisabled(false);
   });
 
-  async function onSubmit(data: z.infer<typeof formSchema>) {
-    const res = await fetch(
-      `${process.env.NEXT_PUBLIC_BACKEND_URL}/createUserByUsernameAndEmail`,
-      {
-        method: "Post",
-        body: JSON.stringify(data),
-        headers: { "Content-Type": "application/json" },
-      },
-    );
-    console.log(await res.json());
-  }
-
   const [showPassword, setShowPassword] = useState(false);
+  const [isRegisterErrorDialogOpen, setIsRegisterErrorDialogOpen] =
+    useState(false);
 
   return (
     <div className="flex min-h-screen w-full items-center justify-center">
+      <Dialog
+        open={isRegisterErrorDialogOpen}
+        onOpenChange={setIsRegisterErrorDialogOpen}
+      >
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>
+              <DialogTitle className="text-xl font-bold">
+                Account Registration Failed
+              </DialogTitle>
+            </DialogTitle>
+            <DialogDescription>
+              Something unexpected happened while trying to create your account,
+              please try again.
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+
       <Card className="gap-3">
         <CardHeader>
           <CardTitle>Register for an Account</CardTitle>
         </CardHeader>
-        <CardContent>
-          <Controller
-            name="username"
-            control={form.control}
-            render={({ field, fieldState }) => (
-              <Field data-invalid={fieldState.invalid}>
-                <FieldLabel htmlFor={field.name}>Username</FieldLabel>
-                <Input
-                  {...field}
-                  className="lg:min-w-md"
-                  id={field.name}
-                  aria-invalid={fieldState.invalid}
-                  placeholder="Username"
-                  autoComplete="off"
-                />
-                {fieldState.invalid && (
-                  <FieldError errors={[fieldState.error]} />
-                )}
-              </Field>
-            )}
-          />
-        </CardContent>
-        <CardContent>
-          <Controller
-            name="email"
-            control={form.control}
-            render={({ field, fieldState }) => (
-              <Field data-invalid={fieldState.invalid}>
-                <FieldLabel htmlFor={field.name}>Email</FieldLabel>
-                <Input
-                  {...field}
-                  id={field.name}
-                  aria-invalid={fieldState.invalid}
-                  placeholder="Email"
-                  autoComplete="off"
-                />
-                {fieldState.invalid && (
-                  <FieldError errors={[fieldState.error]} />
-                )}
-              </Field>
-            )}
-          />
-        </CardContent>
-        <CardContent>
-          <Controller
-            name="password"
-            control={form.control}
-            render={({ field, fieldState }) => (
-              <Field data-invalid={fieldState.invalid}>
-                <FieldLabel htmlFor={field.name}>Password</FieldLabel>
-                <Input
-                  {...field}
-                  id={field.name}
-                  type={showPassword ? "text" : "password"}
-                  aria-invalid={fieldState.invalid}
-                  placeholder="Password"
-                  autoComplete="off"
-                />
-                {fieldState.invalid && (
-                  <FieldError errors={[fieldState.error]} />
-                )}
-                <div className="flex items-center gap-3">
-                  <Checkbox
-                    checked={showPassword}
-                    onCheckedChange={(prev) => {
-                      setShowPassword(!!prev);
-                    }}
+        <form id={formId} onSubmit={formSubmitHandler}>
+          <CardContent>
+            <Controller
+              name="username"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor={field.name}>Username</FieldLabel>
+                  <Input
+                    {...field}
+                    className="lg:min-w-md"
+                    id={field.name}
+                    aria-invalid={fieldState.invalid}
+                    placeholder="Username"
+                    autoComplete="off"
                   />
-                  <Label>Show Password</Label>
-                </div>
-              </Field>
-            )}
-          />
-        </CardContent>
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+          </CardContent>
+          <CardContent>
+            <Controller
+              name="email"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor={field.name}>Email</FieldLabel>
+                  <Input
+                    {...field}
+                    id={field.name}
+                    aria-invalid={fieldState.invalid}
+                    placeholder="Email"
+                    autoComplete="off"
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+          </CardContent>
+          <CardContent>
+            <Controller
+              name="password"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor={field.name}>Password</FieldLabel>
+                  <Input
+                    {...field}
+                    id={field.name}
+                    type={showPassword ? "text" : "password"}
+                    aria-invalid={fieldState.invalid}
+                    placeholder="Password"
+                    autoComplete="off"
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                  <div className="flex items-center gap-3">
+                    <Checkbox
+                      checked={showPassword}
+                      onCheckedChange={(prev) => {
+                        setShowPassword(!!prev);
+                      }}
+                    />
+                    <Label>Show Password</Label>
+                  </div>
+                </Field>
+              )}
+            />
+          </CardContent>
+        </form>
         <CardContent>
-          <Button onClick={form.handleSubmit(onSubmit)}>Submit</Button>
+          <Button
+            disabled={!form.formState.isReady || disabled}
+            type="submit"
+            form={formId}
+          >
+            {!disabled ? "Submit" : <Spinner />}
+          </Button>
         </CardContent>
       </Card>
     </div>
